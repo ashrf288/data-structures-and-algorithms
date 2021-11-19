ba8540ba8ffd84953b7c86cd22f657ce6c1378f4
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-11-19 17:19:32 +0200

    a

--- python/challenges/hashtable/README.md
+++ python/challenges/hashtable/README.md
@@ -1,43 +1,43 @@
 # Hashtables
+
 <!-- Short summary or background information -->
 
 ## Challenge
-<!-- Description of the challenge -->
 
+<!-- Description of the challenge -->
 
 ## Implement a Hashtable Class with the following methods:
 
-+ add
- Arguments: key, value
+-   add
+    Arguments: key, value
 
- Returns: nothing
+Returns: nothing
 
- This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
 
-+ get
- Arguments: key
+-   get
+    Arguments: key
 
 Returns: Value associated with that key in the table
 
-+ contains
+-   contains
 
 Arguments: key
 
 Returns: Boolean, indicating if the key exists in the table already.
 
-+ hash
+-   hash
 
 Arguments: key
 
 Returns: Index in the collection for that key
 
 ## Approach & Efficiency
+
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
-
-
 
 time O(n)
 
-
 ## API
+
 <!-- Description of each method publicly available in each of your hashtable -->
--- python/tests/test_hashTable.py
+++ python/tests/test_hashTable.py
@@ -2,67 +2,71 @@ import pytest
 from challenges.hashtable.hashTable import HashTable
 
 
-
 @pytest.fixture
 def hashtable():
 
-    hashtable=HashTable()
+    hashtable = HashTable()
     return hashtable
+
 
 def test_hashtable_contains_bare_value(hashtable):
 
-    hashtable.add("name","Hamza")
+    hashtable.add("name", "Hamza")
     assert hashtable.contains("name")
-    
+
+
 def test_hashable_contains_pair_in_a_linked_list(hashtable):
 
     hashtable.add("name", "Hamza")
     hashtable.add("name", "Alan")
     assert hashtable.contains("name")
 
+
 def test_hashtable_add(hashtable):
 
     hashtable.add("name", "Einstein")
-    assert hashtable.contains('name')
+    assert hashtable.contains("name")
+
 
 def test_hashable_add_same_key(hashtable):
-  
+
     hashtable.add("a", "a")
     hashtable.add("a", "a")
     assert hashtable.contains("a")
 
+
 @pytest.mark.parametrize(
-    "key,hashcode",
-    [
-       ("a", 679),
-       ("ab", 341),
-       ("hello", 652),
-       ("something000", 658)
-    ]
+    "key,hashcode", [("a", 679), ("ab", 341), ("hello", 652), ("something000", 658)]
 )
-def test_hashtable_hash(key,hashcode, hashtable):
-   
+def test_hashtable_hash(key, hashcode, hashtable):
+
     assert hashtable._HashTable__hash(key) == hashcode
+
+
 def test_hashtable_get_returns_value(hashtable):
 
     expected = "Hamza"
     hashtable.add("name", "Hamza")
     assert hashtable.get("name") == expected
 
+
 def test_hashtable_get_returns_correct_last_value(hashtable):
- 
+
     expected = "Alan"
     hashtable.add("name", "Hamza")
     hashtable.add("name", "Alan")
     assert hashtable.get("name") == expected
 
+
 def test_hashtable_get_returns_none_if_key_does_not_exist(hashtable):
 
     assert not hashtable.get("name")
 
+
 def test_hashtable_contains_returns_false_if_key_does_not_exist(hashtable):
- 
+
     assert not hashtable.contains("name")
+
 
 def test_hashtable_handles_collisions(hashtable):
     expected = "grey"
