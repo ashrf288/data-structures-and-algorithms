971150ab44da92d4fd78bd188d88204a666c73f2
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-11-28 21:58:26 +0200

    test added

--- python/challenges/graph/graph_breadth_search.py
+++ python/challenges/graph/graph_breadth_search.py
@@ -2,142 +2,180 @@ from collections import deque
 
 
 class Vertex:
-  """
-  Class for Adding a node to the graph
-  Arguments: value
-  Returns: The added node
-  """
-  def __init__(self, value):
     """
-    Initalization for a Vertex to hold a value.
-    
+    A class representing a container.
     """
-    self.value = value
-
-
-
-class Stack:
-  def __init__(self):
-    """
-		The constructor method for the stack class and it initializes the dq property to a new double ended queue instance.
-		"""
-    self.dq = deque()
-  
-  def push(self, value):
-    """
-		Store the passed value in a node and then push the node on top of the stack.
-		
-		PARAMETERS
-		----------
-			value: any
-				The value that will get stored in a node to be pushed on top of the stack.
-		"""
-    self.dq.append(value)
-
-  def pop(self):
-    """
-		Return the top node in a stack.
-		"""
-    self.dq.pop()
-    
+    def __init__(self, value):
+        """
+        The constructor method for the vertex class. Initializes the property value.
+        Args:
+            value (any): The value to be stored in a vertex.
+        """
+        self.value = value
 
 
 class Queue:
-  def __init__(self):
-    self.dq = deque()
-  
-  def enqueue(self, value):
-    self.dq.appendleft(value)
+    """
+    A class representing a queue which is a data structure that utilizes the first in first out access method.
+    """
+    def __init__(self):
+        """
+        The constructor method of the queue class. Initializes the dq property to a deque instance.
+        """
+        self.dq = deque()
 
-  def dequeue(self):
-    self.dq.pop()
+    def enqueue(self, value):
+        """
+        Push or add a value after storing it in a vertex to a queue.
+        Args:
+            value (any): The value to be pushed to the queue in a vertex instance.
+        """
+        self.dq.appendleft(value)
 
-  def __len__(self):
-    return len(self.dq)
+    def dequeue(self):
+        """
+        Get or pop the front or the very first element in a queue.
+        """
+        return self.dq.pop()
+
+    def __len__(self):
+        """
+        Overwrite the len method so that the queue has a length property.
+        Returns:
+            int: A representation of the length of a queue.
+        """
+        return len(self.dq)
+
+
+class Stack:
+    """
+    A data structure that utilitzes the first in last out access method.
+    """
+    def __init__(self):
+        """
+		The constructor method for the stack class and it initializes the dq property to a new double ended queue instance.
+		"""
+        self.dq = deque()
+
+    def push(self, value):
+        """
+        Push a value in a node on top of a stack.
+        Args:
+            value (any): The value to be pushed on top of the stack.
+        """
+        self.dq.append(value)
+
+    def pop(self):
+        """
+        Pop or get the very last item in a stack.
+        """
+        self.dq.pop()
+
+
 class Edge:
-  """ 
-    a class for Adding a new edge between two nodes in the graph
-    If specified, assigning a weight to the edge
-    Arguments: 2 nodes to be connected by the edge, weight (optional)
-    Returns: nothing
-    
-  """
-  def __init__(self, vertex, weight):
-    self.vertex = vertex
-    self.weight = weight
+    """
+    A class representing a connection between two enteties or vertices.
+    """
+    def __init__(self, vertex, weight):
+        """
+        The constructor method of edge class. Initializes the vertex and the weight properties.
+        Args:
+            vertex (Vertex): A vertex instance.
+            weight (int): The weight given to a connection.
+        """
+        self.vertex = vertex
+        self.weight = weight
+
 
 class Graph:
-  def __init__(self):
     """
-    Initalization for a hashmap to hold the vertices
+    A class representing a data structure that consists of a set of vertices that are connected to each other.
     """
-    self.__adjacency_list = {}
-    
-  def add_node(self, value):
-    """
-      Method for Adding a node to the graph
-      Arguments: value
-      Returns: The added node
-    """
-    # new node
-    v = Vertex(value)
-    self.__adjacency_list[v] = []
-    return v
+    def __init__(self):
+        """
+        A constructor method of the class Graph. Initialize the adjacency_list property.
+        """
+        self.__adjacency_list = {}
 
-  def size(self):
-    return len(self.__adjacency_list)
-  
-  def add_edge(self, start_vertex, end_vertex, weight=0):
-    """Adds an edge to the graph"""
-    if start_vertex not in self.__adjacency_list:
-      raise KeyError("Start Vertex not found in graph")
-      
-    if end_vertex not in self.__adjacency_list:
-      raise KeyError("End Vertex not found in graph")
+    def add_node(self, value):
+        """
+        Add a node to a graph.
+        Args:
+            value (any): The value to be stored in a vertex to be added to a graph.
+        Returns:
+            vertex: The newly added vertex instance.
+        """
+        v = Vertex(value)
+        self.__adjacency_list[v] = []
+        return v
 
-    edge = Edge(end_vertex, weight)
-    self.__adjacency_list[start_vertex].append(edge)
+    def size(self):
+        """
+        Get the number of vertices in a graph.
+        Returns:
+            int: The number of nodes or vertices in a graph.
+        """
+        return len(self.__adjacency_list)
 
-  def get_nodes(self):
-    """
-    Method to get all nodes in Graph
-    Arguments: None
-    return: All nodes
-    """
-    return self.__adjacency_list.keys()
+    def add_edge(self, start_vertex, end_vertex, weight=0):
+        """
+        Create a connection between two vertices in a graph with a weight.
+        Args:
+            start_vertex (vertex): The starting vertex.
+            end_vertex (vertex):  The ending vertex.
+            weight (int, optional): The weight or strenght of connection. Defaults to 0.
+        Raises:
+            KeyError: If either the start or the end vertex do not exist in the graph.
+        """
+        if start_vertex not in self.__adjacency_list:
+            raise KeyError("Start Vertex not found in graph")
 
-  def get_neighbors(self, vertex):
-    """ """
-    return self.__adjacency_list.get(vertex, [])
-     
+        if end_vertex not in self.__adjacency_list:
+            raise KeyError("End Vertex not found in graph")
 
-  def breath_first_search(self,start_vertex):
-    output=[]
-    queue = Queue()
-    visited = set()
-    queue.enqueue(start_vertex)
-    visited.add(start_vertex)
-    while len(queue):
-      current_vertex = queue.dequeue()
-      output.append(current_vertex)
-      for i in self.__adjacency_list[current_vertex]:
-        if i[0] not in visited:
-          i=i[0]
-          visited.add(i)
-          queue.enqueue(i)
-    return output
+        edge = Edge(end_vertex, weight)
+        self.__adjacency_list[start_vertex].append(edge)
 
+    def get_nodes(self):
+        """
+        Return all the nodes in a graph.
+        Returns:
+            list: A list of all the vertices in a graph.
+        """
+        return self.__adjacency_list.keys()
 
-  
+    def get_neighbors(self, vertex):
+        """
+        Get all the neighbors of the vertex vertex.
+        Args:
+            vertex (vertex): The vertex to get its neighbors.
+        Returns:
+            list: A list of all the adjacent vertices to a given vertex instance.
+        """
+        return self.__adjacency_list.get(vertex, [])
 
-
-
-
-
-
-
-
-    
-
-
-
+    def breadth_first_search(self, start_vertex):
+        """
+        Traverse a graph in breadth first search manner.
+        Args:
+            start_vertex (vertex): the vertex to start traversing from.
+            action (function, optional): the action we want to execute or apply on each vertex in a graph. Defaults to (lambda vertex: None).
+        """
+        queue = Queue()
+        visited = set()
+        nodes = []
+        
+        queue.enqueue(start_vertex)
+        visited.add(start_vertex)
+        
+        while len(queue):
+            current_vertex = queue.dequeue()
+            nodes.append(current_vertex.value)
+            neighbors = self.get_neighbors(current_vertex)
+            print(neighbors)
+            for edge in neighbors:
+                neighbor = edge.vertex
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.enqueue(neighbor)
+        return nodes
--- python/tests/test_graph.py
+++ python/tests/test_graph.py
@@ -2,6 +2,10 @@ import pytest
 
 from challenges.graph.graph import  Graph, Vertex
 
+
+
+
+
 def test_add_node():
   graph = Graph()
   expected = "test"
@@ -93,24 +97,3 @@ def test_get_neighbors():
 
 
 
-def test_graph_depth_first():
-  graph = Graph()
-  v1 = graph.add_node('Pandora')
-  v2 = graph.add_node('Arendelle')
-  v3 = graph.add_node('Metroville')
-  v4 = graph.add_node('Monstroplolis')
-  v5 = graph.add_node('Narnia')
-  v6 = graph.add_node('Naboo')
-  graph.add_edge(v1, v2)
-  graph.add_edge(v1, v4)
-  graph.add_edge(v2,v1)
-  graph.add_edge(v2,v3)
-  graph.add_edge(v2,v4)
-  graph.add_edge(v3,v2)
-  graph.add_edge(v4,v1)
-  graph.add_edge(v4,v2)
-  graph.add_edge(v4,v5)
-  graph.add_edge(v4,v6)
-  graph.add_edge(v6,v4)
-  graph.add_edge(v5,v4)
-  assert["Pandora", "Arendelle", "Metroville", "Monstroplolis", "Narnia", "Naboo"] == ["Pandora", "Arendelle", "Metroville", "Monstroplolis", "Narnia", "Naboo"]
